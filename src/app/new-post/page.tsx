2bb6bf0631719089e1c645256b46ccdd800906b2
sanixto <sanix.to@gmail.com>
2024-05-03 16:28:04 +0300

    add post form validation

--- src/app/new-post/page.tsx
+++ src/app/new-post/page.tsx
@@ -1,14 +1,30 @@
-import styles from './page.module.css';
-import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
-import FormSubmit from '@/components/form-submit';
+
+import { storePost } from '@/lib/posts';
+import PostForm from '@/components/post-form';
 
 export default function NewPostPage() {
-  async function createPost(formData: FormData) {
+  async function createPost(prevState: { errorMessages: string[] }, formData: FormData): Promise<{ errorMessages: string[] }> {
     'use server';
-    const title = formData.get('title')?.toString()!;
-    const image = formData.get('image');
-    const content = formData.get('content')?.toString()!;
+    const title: string = formData.get('title')?.toString()!;
+    const image: File = formData.get('image') as File;
+    const content: string = formData.get('content')?.toString()!;
+
+    const errorMessages: string[] = [];
+
+    if (!title || title.trim().length === 0) {
+      errorMessages.push('Title is required.')
+    }
+
+    if (!content || content.trim().length === 0) {
+      errorMessages.push('Content is required.')
+    }
+
+    if (!image || image.size === 0) {
+      errorMessages.push('Image is required.')
+    }
+
+    if (errorMessages.length > 0) return { errorMessages };
 
     storePost({
       imageUrl: '',
@@ -19,32 +35,10 @@ export default function NewPostPage() {
 
     redirect('/feed');
   }
-
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className={styles["form-control"]}>
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className={styles["form-control"]}>
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className={styles["form-control"]}>
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows={5} />
-        </p>
-        <p className={styles["form-actions"]}>
-          <FormSubmit />
-        </p>
-      </form>
+      <PostForm action={createPost} />
     </>
   );
 }
---
+++ src/components/post-form/index.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+import FormSubmit from '../form-submit';
+import styles from './index.module.css';
+
+interface PostFormProps {
+  action: (prevState: { errorMessages: string[] }, formData: FormData) => Promise<{ errorMessages: string[] }>,
+}
+
+export default function PostForm({ action }: PostFormProps) {
+  const [state, formAction] = useFormState(action, {errorMessages: []});
+  return (
+    <form action={formAction}>
+      <p className={styles["form-control"]}>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" required />
+      </p>
+      <p className={styles["form-control"]}>
+        <label htmlFor="image">Image URL</label>
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          id="image"
+          name="image"
+          required
+        />
+      </p>
+      <p className={styles["form-control"]}>
+        <label htmlFor="content">Content</label>
+        <textarea id="content" name="content" rows={5} required />
+      </p>
+      <p className={styles["form-actions"]}>
+        <FormSubmit />
+      </p>
+      {state.errorMessages.length > 0 && (
+        <ul className={styles["form-errors"]}>
+          {state.errorMessages.map(errorMsg => (
+            <li key={errorMsg}>{errorMsg}</li>
+          ))}
+        </ul>
+      )}
+    </form>
+  );
+}
