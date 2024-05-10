85bc1d11fe937eedf69d446eeabae19e41ba03a9
sanixto <sanix.to@gmail.com>
2024-05-10 20:55:59 +0300

    optimize logo using Image component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,12 +2,13 @@ import logo from '@/assets/logo.png';
 import Link from 'next/link';
 
 import styles from './index.module.css';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image src={logo} alt="Mobile phone with posts feed on it" priority />
       </Link>
       <nav>
         <ul>
